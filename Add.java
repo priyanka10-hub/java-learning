18f8e2f8c2973e2e44cb9a1e31a7285e1155c1e6
priyanka10-hub <keraipriyanka0@gmail.com>
2026-08-02 13:35:09 -0400

    Add.java

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+class Add{
+
+public static void main(String args[])
+{
+
+int num1 = 5;
+int num2 = 2;
+int num3 = 3;
+int sum = num1+num2+num3;
+
+System.out.println("The sum is = "+sum);
+}
+}
